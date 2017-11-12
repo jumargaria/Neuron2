aad2b50e6aef6e0a4ffcbba2a8b3a54ff605e4d5
jumargaria <32436750+jumargaria@users.noreply.github.com>
2017-11-12 11:38:53 +0100

    Update constants.hpp

--- constants.hpp
+++ constants.hpp
@@ -18,14 +18,10 @@ unsigned long const NI(2500); //!number of connection of inhibitory neurone
 unsigned long  const CE(NE*0.1);//!<connections from the excitatory neurons in the network
 unsigned long  const CI(NI*0.1);//!<connections from the inhibitory neurons in the network
 unsigned long  const C_EXT(CE);//!< number of connections from outside the network
-<<<<<<< HEAD
 double const g(5);//! JI/JE
-=======
-double const g(6);//! JI/JE
->>>>>>> 4272d06dbac65653125863db7420ad3f83fbb81d
 double const JE(0.1);//! constante of weight of connection of an excitatory neurone
 double const JI(-g*JE);//! constant of weight of connection of an inhibitory neurone
-unsigned long const eta(4); //!
+unsigned long const eta(2); //!
 double const NU_thr(THRESHOLD/ (CE*JE*TAU));//!stimulus par ms
 double const NU_ext(eta*NU_thr*C_EXT);//!additional random input from the rest of the brain.
 
